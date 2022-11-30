c95edb17d8358becd082bf6bb269738cb0f6e59c
jneda <julien.dane@gmail.com>
2022-11-30 17:56:16 +0100

    Appel au routeur dans index.php

--- index.php
+++ index.php
@@ -3,11 +3,19 @@
 $configFile = file_get_contents("config/config.json");
 $config = json_decode($configFile);
 
-spl_autoload_register(function($class) use ($config) {
-  foreach($config->autoloadFolders as $folder) {
+spl_autoload_register(function ($class) use ($config) {
+  foreach ($config->autoloadFolders as $folder) {
     if (file_exists($folder . '/' . $class . '.php')) {
       require_once($folder . '/' . $class . '.php');
       break;
     }
   }
 });
+
+try {
+  $httpRequest = new HttpRequest();
+  $router = new Router();
+  $httpRequest->setRoute($router->findRoute($httpRequest));
+} catch (Exception $e) {
+  echo "An error occurred";
+}
