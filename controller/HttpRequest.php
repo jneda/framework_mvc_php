5b652f7e5aab84118dcd6e09ce27e569470e6541
jneda <julien.dane@gmail.com>
2022-11-30 18:09:47 +0100

    La classe HttpRequest est capable de récupérer les paramètres de la requête

--- controller/HttpRequest.php
+++ controller/HttpRequest.php
@@ -11,6 +11,7 @@ class HttpRequest
   {
     $this->url = $_SERVER['REQUEST_URI'];
     $this->method = $_SERVER['REQUEST_METHOD'];
+    $this->params = array();
   }
 
   public function getUrl()
@@ -31,5 +32,27 @@ class HttpRequest
   public function setRoute($route)
   {
     $this->route = $route;
+  }
+
+  public function bindParam()
+  {
+    switch($this->method)
+    {
+      case "GET":
+      case "DELETE":
+        if (preg_match("#" . $this->route->path . "#", $this->url, $matches)) {
+          for ($i = 1; $i < count($matches); $i++) {
+            $this->params[] = $matches[$i];
+          }
+        }
+      
+      case "POST":
+      case "PUT":
+        foreach($this->route->getParams as $param) {
+          if (isset($_POST[$param])) {
+            $this->params[] = $_POST[$param];
+          }
+        }
+    }
   }
 }
